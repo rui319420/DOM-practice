6ad4312994fe71bb602b39b1024cb06bc95f2f5e
Rui <s1F102501866@iniad.org>
2025-11-12 23:53:00 +0900

    feat: install morgan

--- Node.js-basics/2/app.js
+++ Node.js-basics/2/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -8,6 +9,22 @@ app.set('view engine', 'ejs');
 
 // listen for request
 app.listen(3000);
+
+app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//   console.log('new request made');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// })
+
+// app.use((req, res, next) => {
+//   console.log('new request made');
+//   console.log('in the nexr middleware')
+//   next();
+// })
 
 app.get('/', (req, res) => {
   // res.send('<p>home page</p>');
