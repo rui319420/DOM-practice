cc528999a2d22bd2c4559ecf918b7104e8e9c921
Rui <s1F102501866@iniad.org>
2025-11-20 23:27:38 +0900

    feat: disable unselect cards

--- DOM/DOM.js
+++ DOM/DOM.js
@@ -1,10 +1,7 @@
 const elem = document.getElementById('elem');
 elem.addEventListener('mousemove', runEvent);
 
-const block = document.getElementById('elem')
-
-document.block.style.backgroundColor = '#f4f4f4'
-
+// イベントが発生したときの処理
 function runEvent(e) {
   console.log(e.offsetX, e.offsetY);
   document.getElementById('elem').style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",40)"
--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // shuffle cards
   const shuffleCards = () => {
@@ -23,6 +24,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
     
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffleCards);
     setTurns(0);
   }
@@ -34,7 +37,7 @@ function App() {
 
   useEffect(() => {
     if (choiceOne && choiceTwo) {
-
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -59,7 +62,12 @@ function App() {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className="App">
@@ -72,10 +80,12 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
-            flipped ={card === choiceOne || card === choiceTwo || card.matched}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
